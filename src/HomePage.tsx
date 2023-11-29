1009ffda5406f1c08c83d1c5e6be2c35c8916e14
seanflynn5 <seanflynn5000@yahoo.com>
2023-11-29 10:27:02 -0500

    implement react-fast-marquee

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -6,13 +6,12 @@ import MyNavbar from './MyNavbar.tsx';
 import ExperienceModal from './ExperienceModal.tsx';
 import ProjectModal from './ProjectsModal.tsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Marquee from "react-fast-marquee";
 import './App.css';
 
 function HomePage(): JSX.Element {
   const [expModalShow, setExpModalShow] = useState(false);
   const [projModalShow, setProjModalShow] = useState(false);
-
-  const skills: string[] = ['JavaScript','TypeScript','C++','React','Vue','HTML', 'CSS', 'Node.js', 'Express', 'MongoDB', 'PostgreSQL','OAuth','Docker','Git'];
 
   return (
     <>
@@ -26,14 +25,8 @@ function HomePage(): JSX.Element {
         <p className="intro">
           Hi! 👋  I'm Sean, a software engineer with experience in fullstack development. Starting in 2024, I will be working at <a href='https://www.bradyid.com/'>Brady Corporation</a> as a Web Developer, and recently worked at <a href='https://www.codesmith.io/'>Codesmith</a> as a Software Engineer. In my downtime, I enjoy playing and listening to music (as you can tell by my photo!). Let's be in touch!
         </p>
-        <h4>Skills & Technologies</h4>
-        <div id='skills-box'>
-          <div className='skills-container'>
-            {skills.map((skill, index) => (
-           <div key={index} className='skill-item'>{skill}</div>
-             ))}
-          </div>
-      </div>
+        <h5>Skills & Technologies</h5>
+      <Marquee autoFill={true} speed={30} className='skills-box'> JavaScript TypeScript C++ React Vue HTML CSS Node.js Express MongoDB PostgreSQL OAuth Jest Docker Git&nbsp;</Marquee>
       </div>
       <div id='modal-btn-container'>
         <button className='modal-btn' onClick={() => setExpModalShow(true)}>
